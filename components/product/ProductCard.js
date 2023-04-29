8b042d9457371c12cadd8154f61fa71e935efba2
Al Arafat Siddique <alarafatsiddique@gmail.com>
2023-04-29 23:33:50 +0600

    product details page

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -1,5 +1,6 @@
 import { addToCart } from '@/store/cartSlice';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 import { useDispatch } from 'react-redux';
 
@@ -10,7 +11,7 @@ const ProductCard = ({data}) => {
     <div className="product d-flex flex-column overflow-hidden">
     <figure className="mb-0 product-media bg-white d-flex justify-content-center align-items-center">
       <span className="product-label label-sale">SALE</span>
-      <a href="product.html" className="w-100">
+      <Link href={`/product/${p.slug}`} className="w-100">
         <Image
           src={p?.thumbnail?.data?.attributes?.url}
           alt="Product image"
@@ -19,46 +20,18 @@ const ProductCard = ({data}) => {
           height={239}
         />
        
-      </a>
-      <div
-        className="product-countdown bg-light"
-        data-until="+55h"
-        data-relative="true"
-        data-labels-short="true"
-      />
-      {/* End .product-countdown */}
-      {/* <div className="product-action-vertical">
-        <a
-          href="#"
-          className="btn-product-icon text-dark btn-wishlist"
-          title="Add to wishlist"
-        >
-          <span>add to wishlist</span>
-        </a>
-        <a
-          href="popup/quickView.html"
-          className="btn-product-icon text-dark btn-quickview"
-          title="Quick view"
-        >
-          <span>Quick view</span>
-        </a>
-        <a
-          href="#"
-          className="btn-product-icon text-dark btn-compare"
-          title="Compare"
-        >
-          <span>Compare</span>
-        </a>
-      </div> */}
+      </Link>
+
     </figure>
-    {/* End .product-media bg-white d-flex justify-content-center align-items-center */}
+
     <div className="product-body pb-3">
       <div className="text-left product-cat font-weight-normal text-light mb-0">
         <a href="#">  {p.category.data.attributes.name}</a>
       </div>
       {/* End .product-cat  */}
       <h3 className="product-title letter-spacing-normal font-size-normal text-left mb-0">
-        <a href="product.html">{p.title}</a>
+      <Link href={`/product/${p.slug}`}>{p.title}</Link>
+
       </h3>
       {/* End .product-title letter-spacing-normal font-size-normal */}
       <div className="product-price mb-1">
@@ -83,17 +56,6 @@ const ProductCard = ({data}) => {
         <span className="text-dark shadow-none">add to cart</span>
       </button>
     </div>
-    {/* <div className="product-sold">
-      <div className="sold position-relative w-100">
-        <div
-          className="sold-val position-absolute"
-          style={{ width: "80%" }}
-        />
-        <span className="sold-text font-size-normal second-primary-color">
-          Sold: 54
-        </span>
-      </div>
-    </div> */}
   </div>
   )
 }
---
+++ pages/product/[slug].js
@@ -0,0 +1,211 @@
+import React from "react";
+import { fetchDataFromApi } from "@/utils/api";
+import Image from "next/image";
+import Link from "next/link";
+import ReactMarkdown from "react-markdown";
+import { useDispatch } from "react-redux";
+import { addToCart } from "@/store/cartSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ProductDetails = ({ product, products }) => {
+  // console.log(product);
+  const p = product?.data?.[0]?.attributes;
+  const dispatch = useDispatch();
+
+  return (
+    <main className="main">
+      <nav aria-label="breadcrumb" className="breadcrumb-nav border-0 mb-0">
+        <div className="container d-flex align-items-center">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="#">Products</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Default
+            </li>
+          </ol>
+        </div>
+      </nav>
+      <ToastContainer/>
+
+      <div className="page-content">
+        <div className="container">
+          <div className="product-details-top">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="product-gallery product-gallery-vertical">
+                  <div className="row">
+                    <figure className="product-main-image">
+                      <Image
+                        id="product-zoom"
+                        src={p?.thumbnail?.data?.attributes?.url}
+                        data-zoom-image={p?.thumbnail?.data?.attributes?.url}
+                        width={300}
+                        height={300}
+                        alt={p?.title}
+                        priority="true"
+                      />
+                      <a
+                        href="#"
+                        id="btn-product-gallery"
+                        className="btn-product-gallery"
+                      >
+                        <i className="icon-arrows" />
+                      </a>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="product-details">
+                  <h1 className="product-title">{p?.title}</h1>
+
+                  <div className="product-price">{p?.price}BDT</div>
+                  <div className="product-content">
+                    <p>{p?.short_description}</p>
+                  </div>
+                  {/* 
+              <div className="details-filter-row details-row-size">
+                <label htmlFor="size">Size:</label>
+                <div className="select-custom">
+                  <select name="size" id="size" className="form-control">
+                    <option value="#" selected="selected">
+                      Select a size
+                    </option>
+                    <option value="s">Small</option>
+                    <option value="m">Medium</option>
+                    <option value="l">Large</option>
+                    <option value="xl">Extra Large</option>
+                  </select>
+                </div>
+               
+              
+              </div> */}
+                  <div className="details-filter-row details-row-size">
+                    <label htmlFor="qty">Qty:</label>
+                    <div className="product-details-quantity">
+                      <input
+                        type="number"
+                        id="qty"
+                        className="form-control"
+                        defaultValue={1}
+                        min={1}
+                        max={10}
+                        step={1}
+                        data-decimals={0}
+                        required=""
+                      />
+                    </div>
+                  </div>
+                  <div className="product-details-action">
+                    <button className="btn-product btn-cart"  onClick={()=>{
+            dispatch(addToCart({
+              ...product?.data?.[0],
+              oneQuantityPrice: p?.price
+            }));
+            toast.success("Product Added to Cart", {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+           
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+              });
+           }}>
+                      <span>add to cart</span>
+                    </button>
+
+                  </div>
+                  <div className="product-details-footer">
+                    <div className="product-cat">
+                      <span>Category:</span>
+
+                      <Link
+                        href={`/category/${p?.category?.data?.attributes?.slug}`}
+                      >
+                        {" "}
+                        {p?.category?.data?.attributes?.name}
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="product-details-tab">
+            <ul className="nav nav-pills justify-content-start" role="tablist">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  id="product-desc-link"
+                  data-toggle="tab"
+                  href="#product-desc-tab"
+                  role="tab"
+                  aria-controls="product-desc-tab"
+                  aria-selected="true"
+                >
+                  Description
+                </a>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div
+                className="tab-pane fade show active"
+                id="product-desc-tab"
+                role="tabpanel"
+                aria-labelledby="product-desc-link"
+              >
+                <div className="product-desc-content">
+                  <h3>Product Information</h3>
+                  <p>
+                    <ReactMarkdown>{p?.description}</ReactMarkdown>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ProductDetails;
+
+export async function getStaticPaths() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+  const paths = products?.data?.map((p) => ({
+    params: {
+      slug: p.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps({ params: { slug } }) {
+  const product = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+  );
+
+  return {
+    props: {
+      product,
+      products,
+      slug,
+    },
+  };
+}
