3b45f76676cb3abe4486c3c22736d16c7376191f
msajiba <asajib499@gmail.com>
2023-05-13 23:23:58 +0600

    extra page added

---
+++ pages/contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='container'>
+            <h3> Looking Forward to Hear From You </h3> 
+            <p> Getting in contact with SafeFoods.com.bd is super easy! Follow any</p>
+        </div>
+    );
+};
+
+export default Contact;
