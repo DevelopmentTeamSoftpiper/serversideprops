4766151729244cf13eb99e2b9b026e02df891ad7
msajiba <asajib499@gmail.com>
2023-06-03 15:48:04 +0600

    blog and sub blog complete

---
+++ pages/api/admin/blog/update.js
@@ -0,0 +1,40 @@
+import { createRouter } from "next-connect";
+import { verifyTokenAndAdmin } from "@/helpers/verityToken";
+import db from "@/utils/db";
+import Category from "@/models/Category";
+import slugify from "slugify";
+import applyCors from "@/middleware/cors";
+import Blog from "@/models/Blog";
+
+const router = createRouter().use(verifyTokenAndAdmin);
+
+router.post(async (req, res) => {
+  try {
+    const { title, image, author, content, subBlog, id } = req.body;
+    db.connectDb();
+    const updated = await Blog.findByIdAndUpdate(id, {
+      title,
+      slug: slugify(title.toString()),
+      image,
+      author,
+      content,
+      subBlog,
+    });
+    db.disconnectDb();
+    if (updated) {
+      return res.json({
+        status: true,
+        message: "Blog has been updated successfully",
+      });
+    } else {
+      return res.json({
+        status: false,
+        message: "Blog not found with this id",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default applyCors(router.handler());
