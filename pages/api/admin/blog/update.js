5765f490a9caa423c315cafda90ed13923184b68
msajiba <asajib499@gmail.com>
2023-06-03 18:51:01 +0600

    update

--- pages/api/admin/blog/update.js
+++ pages/api/admin/blog/update.js
@@ -1,7 +1,6 @@
 import { createRouter } from "next-connect";
 import { verifyTokenAndAdmin } from "@/helpers/verityToken";
 import db from "@/utils/db";
-import Category from "@/models/Category";
 import slugify from "slugify";
 import applyCors from "@/middleware/cors";
 import Blog from "@/models/Blog";
@@ -38,3 +37,4 @@ router.post(async (req, res) => {
 });
 
 export default applyCors(router.handler());
+  
